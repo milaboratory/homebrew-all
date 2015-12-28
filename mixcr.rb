79460cb852f5a71cb87861ab714f80d953df2877
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2015-12-28 18:39:43 +0300

    Comment out "desc" to support old version of brew.

--- mixcr.rb
+++ mixcr.rb
@@ -1,5 +1,5 @@
 class Mixcr < Formula
-  desc "MiXCR is a universal software for fast and accurate analysis of T- and B- cell receptor repertoire sequencing data."
+  # desc "MiXCR is a universal software for fast and accurate analysis of T- and B- cell receptor repertoire sequencing data."
   homepage "http://mixcr.milaboratory.com/"
   url "https://github.com/milaboratory/mixcr/releases/download/v1.6/mixcr-1.6.zip"
   version "1.6"
