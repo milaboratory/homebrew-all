72019847f6b764036edfe5e8035ffb4417926214
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2016-09-13 19:50:32 +0300

    MiXCR v2.0

--- mixcr.rb
+++ mixcr.rb
@@ -1,9 +1,9 @@
 class Mixcr < Formula
   # desc "MiXCR is a universal software for fast and accurate analysis of T- and B- cell receptor repertoire sequencing data."
   homepage "http://mixcr.milaboratory.com/"
-  url "https://github.com/milaboratory/mixcr/releases/download/v1.8.3/mixcr-1.8.3.zip"
-  version "1.8.3-1"
-  sha256 "dcef392f94caa3986ce51b71116025d8241831e968bba8f17958e126f058b798"
+  url "https://github.com/milaboratory/mixcr/releases/download/v2.0/mixcr-2.0.zip"
+  version "2.0.0-1"
+  sha256 "171baa889d5acf5e183ab46fe4d2ccd663d43dcc3df172e4d4c087f24118eb46"
 
   depends_on :java
 
