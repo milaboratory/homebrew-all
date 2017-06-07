8c4029a90b24c47421b30ece24766c1386f414f8
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2017-06-07 18:35:40 +0300

    MiTools 1.5

--- mitools.rb
+++ mitools.rb
@@ -1,8 +1,8 @@
 class Mitools < Formula
   homepage "https://github.com/milaboratory/mitools"
-  url "https://github.com/milaboratory/mitools/releases/download/v1.4/mitools-1.4.zip"
-  version "1.4.0-1"
-  sha256 "060b5efc49b08938c42279945f5c4c35b91815f810ea2ad4b636ef90b7d3b865"
+  url "https://github.com/milaboratory/mitools/releases/download/v1.5/mitools-1.5.zip"
+  version "1.5.0-1"
+  sha256 "23a3b20730ce5b66c3acb966b63568895385399fbc7df2f8cdc5ec2441ed0595"
 
   depends_on :java
 
