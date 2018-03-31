145b2254b115dd6aa4dc2aec703748f56842805d
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2018-04-01 00:19:08 +0300

    MiXCR release 2.1.10, added license notice

--- mixcr.rb
+++ mixcr.rb
@@ -1,15 +1,27 @@
 class Mixcr < Formula
-  # desc "MiXCR is a universal software for fast and accurate analysis of T- and B- cell receptor repertoire sequencing data."
-  homepage "http://mixcr.milaboratory.com/"
-  url "https://github.com/milaboratory/mixcr/releases/download/v2.1.8/mixcr-2.1.8.zip"
-  version "2.1.8-1"
-  sha256 "22cff4c32d850571707d7ab27674894ccb1d912610b3c5d9090bf78acaba00fd"
+  desc "MiXCR is a universal software for fast and accurate analysis of T- and B- cell receptor repertoire sequencing data."
+  homepage "https://mixcr.readthedocs.io/"
+  url "https://github.com/milaboratory/mixcr/releases/download/v2.1.10/mixcr-2.1.10.zip"
+  version "2.1.10-1"
+  sha256 "ca64e2adef72aaedab54d19a89529c3cbc2f2343f5154d9bfd9df892e78ba37d"
 
   depends_on :java
 
   def install
     prefix.install Dir["*"]
     bin.install_symlink "../mixcr"
+  end
+
+  def caveats
+    <<~EOS
+      License notice:
+
+        MiXCR is free for academic and non-commercial use.
+        See: https://github.com/milaboratory/mixcr/blob/develop/LICENSE
+
+        For commertioal use please contact licensing@milaboratory.com
+
+    EOS
   end
 
   test do
