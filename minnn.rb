891ec8fb714eb1ebae816e5ca6867bc43f5257e9
Alexander230 <fire_2005@mail.ru>
2018-08-15 17:02:45 +0300

    MiNNN 1.0.0

---
+++ minnn.rb
@@ -0,0 +1,30 @@
+class Minnn < Formula
+  desc "MiNNN is a universal software for fast and accurate analysis of T- and B- cell receptor repertoire sequencing data."
+  homepage "https://minnn.readthedocs.io/"
+  url "https://github.com/milaboratory/minnn/releases/download/v1.0.0/minnn-1.0.0.zip"
+  version "1.0.0-1"
+  sha256 "712db4a12c23b72c7b21b8d15dcd714afcbc28da3ce002388966f249f219d002"
+
+  depends_on :java
+
+  def install
+    prefix.install Dir["*"]
+    bin.install_symlink "../minnn"
+  end
+
+  def caveats
+    <<~EOS
+      License notice:
+
+        MiNNN is free for academic and non-commercial use.
+        See: https://github.com/milaboratory/minnn/blob/develop/LICENSE
+
+        For commertial use please contact licensing@milaboratory.com
+
+    EOS
+  end
+
+  test do
+    system "minnn"
+  end
+end
