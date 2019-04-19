5b971a19b367d5e1926ddfcb2c0ce4cc10b7a091
Alexander230 <fire_2005@mail.ru>
2019-04-19 15:59:29 +0300

    MiNNN 4.0.1

--- minnn.rb
+++ minnn.rb
@@ -1,9 +1,9 @@
 class Minnn < Formula
   desc "MiNNN is a universal software for fast and accurate analysis of T- and B- cell receptor repertoire sequencing data."
   homepage "https://minnn.readthedocs.io/"
-  url "https://github.com/milaboratory/minnn/releases/download/v4.0.0/minnn-4.0.0.zip"
-  version "4.0.0-1"
-  sha256 "9f2f4e29ae7074f3a4345e5469d363474723abe1823563a5fceb2b13b743ab00"
+  url "https://github.com/milaboratory/minnn/releases/download/v4.0.1/minnn-4.0.1.zip"
+  version "4.0.1-1"
+  sha256 "9dc021966c05e16a0fe32eb447a607f01b264ebf4b20029097a3005e69bf0259"
 
   depends_on :java
 
