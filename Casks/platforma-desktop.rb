6572e246e4cc08923590705bd0a7590c8934c918
pvyazankin <pvyazankin@milaboratories.com>
2025-07-30 19:12:55 +0200

    platforma-desktop: Update to version 1.21.7

--- Casks/platforma-desktop.rb
+++ Casks/platforma-desktop.rb
@@ -1,13 +1,13 @@
 cask "platforma-desktop" do
-  version "1.21.6"
+  version "1.21.7"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://cdn.platforma.bio/software/platforma-desktop-v2/macos/arm64/Platforma-#{version}-arm64.dmg"
-      sha256 "9e851c4b40df49cf2f1ef661d4f5dc39d22b52142216960aaa4d1426951e698d"   ### ARM64 DON'T DELETE
+      sha256 "9a7adae1ea9ed512fc9f475bbc44eabcca93684d64b0a5c1fa1a0abd214c6b76"   ### ARM64 DON'T DELETE
     else
       url "https://cdn.platforma.bio/software/platforma-desktop-v2/macos/amd64/Platforma-#{version}.dmg"
-      sha256 "90e5d8cb145c363b5514be1f5f85380bb793549de08c84b3757dc878c16fd3b4"   ### AMD64 DON'T DELETE
+      sha256 "24818a2f9786c4ae18712579e2f86a3a7e792fe79fc346c25a680209a1ae4da6"   ### AMD64 DON'T DELETE
     end
   app "Platforma.app"
   end
